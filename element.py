6a3fdd8dc8bf2569e59308d26eb47daf6e9a1cf4
Mohammad Shahrad <engshahrad@gmail.com>
2021-11-14 05:48:25 +0000

    new method added to the Element class + tests for Element

--- element.py
+++ element.py
@@ -21,3 +21,11 @@ class Element:
                 return sum( [self.coreUsageMetric[i] for i in range(len(self.types)) if self.types[i]==objectType ] )
             elif (resourceType=='M'):
                 return sum( [self.memUsageMetric[i] for i in range(len(self.types)) if self.types[i]==objectType ] )
+    
+    def GetSplitCommBW(self):
+        splitCommBW = sum( [sum(x) for x in self.communicationMetric] )
+        # remove communication bandwidth of each object to itself
+        splitCommBW -= sum( [self.communicationMetric[i][i] for i in range(len(self.communicationMetric))] )
+        # divide by 2 to eliminate duplicate counting
+        splitCommBW /= 2
+        return splitCommBW
---
+++ tests/ElementTests.py
@@ -0,0 +1,35 @@
+import sys
+import unittest
+sys.path.insert(0, '.')
+sys.path.insert(0, '..')
+from element import Element
+
+class TestElement(unittest.TestCase):
+    e1 = Element(types=['c', 'm'], 
+            coreUsageMetric=[1, 0.2], 
+            memUsageMetric= [52, 245], 
+            communicationMetric=[[0, 120], [120,0]])
+    e2 = Element(types=['c', 'm', 'c'], 
+            coreUsageMetric=[1, 0.2, 2], 
+            memUsageMetric= [52, 245, 48], 
+            communicationMetric=[[1000, 120, 540], [120, 1000, 800], [540, 800, 63]])
+    
+    def test_GetTotalResources(self):
+        core = self.e1.GetTotalResources(resourceType='C')
+        mem = self.e1.GetTotalResources(resourceType='M')
+        self.assertEqual([core, mem], [1.2, 297])
+
+    def test_GetTotalResourcesPerObject(self):
+        coreC = self.e1.GetTotalResources(resourceType='C', objectType='c')
+        memC = self.e1.GetTotalResources(resourceType='M', objectType='c')
+        coreM = self.e1.GetTotalResources(resourceType='C', objectType='m')
+        memM = self.e1.GetTotalResources(resourceType='M', objectType='m')
+        self.assertEqual([coreC, memC, coreM, memM], [1, 52, 0.2, 245])
+    
+    def test_GetSplitCommBW(self):
+        e1Res = self.e1.GetSplitCommBW()
+        e2Res = self.e2.GetSplitCommBW()
+        self.assertEqual([e1Res, e2Res], [120, 1460])
+
+if __name__ == '__main__':
+    unittest.main()
