79e6d35a8360f630faf3833832cd3d3f5d80e737
Mohammad Shahrad <engshahrad@gmail.com>
2021-11-14 01:39:28 +0000

    added constraints for desiredServerPoolReduction so that the splitter does not recommend too many splits -> ideally DAG objects should remain merged for better performance

--- splitter.py
+++ splitter.py
@@ -5,24 +5,28 @@ class Splitter:
                 leftCPUPoolCapacity, leftMemoryPoolCapacity):
         
         model = Model(sense=MAXIMIZE, solver_name=CBC)
-
-        # for i in range(len(splitCandidates)):
-        #     splitCandidate = splitCandidates[i]
         
         x = [model.add_var(var_type=BINARY) for s in splitCandidates]
         model.objective = sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='C') \
                                 + x[i]*splitCandidates[i].GetTotalResources(resourceType='M') \
                                 for i in range(len(x))] )
 
+        # constraints to limit the max splitting, to maintain merged elements for performance
+        model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='C') \
+                                for i in range(len(x))] ) <= desiredServerPoolReduction['cores'])
+        model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='M') \
+                                for i in range(len(x))] ) <= desiredServerPoolReduction['mem'])
+        # constraints to respect resources available on the CPU Pool
         model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='C', objectType='c') \
                                 for i in range(len(x))] ) <= leftCPUPoolCapacity['cores'])
         model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='M', objectType='c') \
                                 for i in range(len(x))] ) <= leftCPUPoolCapacity['mem'])
+        # constraints to respect resources available on the Memory Pool
         model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='C', objectType='m') \
                                 for i in range(len(x))] ) <= leftMemoryPoolCapacity['cores'])
         model.add_constr( sum( [x[i]*splitCandidates[i].GetTotalResources(resourceType='M', objectType='m') \
                                 for i in range(len(x))] ) <= leftMemoryPoolCapacity['mem'])
         
-        status = model.optimize(max_seconds=20)
+        status = model.optimize(max_seconds=30)
         print(status)
         return [x[i].x for i in range(len(x))]
--- tests/SplittingTests.py
+++ tests/SplittingTests.py
@@ -8,6 +8,76 @@ from splitter import Splitter
 class TestSplitting(unittest.TestCase):
     splitter = Splitter()
 
+    def test_NotTooMuchSplittingNeeded1(self):
+        e1 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 0.2], memUsageMetric= [52, 245], 
+            communicationMetric=[[0, 120], [120,0]])
+        e2 = Element(types=['c','m'], 
+            coreUsageMetric=[0.8, 0.5], memUsageMetric= [158, 652], 
+            communicationMetric=[[0, 840], [840,0]])
+        e3 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 1], memUsageMetric= [26, 39], 
+            communicationMetric=[[0, 30], [30,0]])
+        splittingDecisions = self.splitter.SuggestSplit(splitCandidates=[e1, e2, e3], 
+                                                        desiredServerPoolReduction={'cores': 1, 'mem':1024}, 
+                                                        leftCPUPoolCapacity={'cores': 100, 'mem':10240}, 
+                                                        leftMemoryPoolCapacity={'cores': 50, 'mem':40960})
+        # no splitting should be recommended as the minimum split goes beyond desiredServerPoolReduction core limit
+        self.assertEqual(splittingDecisions, [0, 0, 0]) 
+        
+    
+    def test_NotTooMuchSplittingNeeded2(self):
+        e1 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 0.2], memUsageMetric= [52, 245], 
+            communicationMetric=[[0, 120], [120,0]])
+        e2 = Element(types=['c','m'], 
+            coreUsageMetric=[0.8, 0.5], memUsageMetric= [158, 652], 
+            communicationMetric=[[0, 840], [840,0]])
+        e3 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 1], memUsageMetric= [26, 39], 
+            communicationMetric=[[0, 30], [30,0]])
+        splittingDecisions = self.splitter.SuggestSplit(splitCandidates=[e1, e2, e3], 
+                                                        desiredServerPoolReduction={'cores': 20, 'mem':50}, 
+                                                        leftCPUPoolCapacity={'cores': 100, 'mem':10240}, 
+                                                        leftMemoryPoolCapacity={'cores': 50, 'mem':40960})
+        # no splitting should be recommended as the minimum split goes beyond desiredServerPoolReduction memory limit
+        self.assertEqual(splittingDecisions, [0, 0, 0])
+
+    def test_NotTooMuchSplittingNeeded3(self):
+        e1 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 0.2], memUsageMetric= [52, 245], 
+            communicationMetric=[[0, 120], [120,0]])
+        e2 = Element(types=['c','m'], 
+            coreUsageMetric=[0.8, 0.5], memUsageMetric= [158, 652], 
+            communicationMetric=[[0, 840], [840,0]])
+        e3 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 1], memUsageMetric= [26, 39], 
+            communicationMetric=[[0, 30], [30,0]])
+        splittingDecisions = self.splitter.SuggestSplit(splitCandidates=[e1, e2, e3], 
+                                                        desiredServerPoolReduction={'cores': 1.2, 'mem':1024}, 
+                                                        leftCPUPoolCapacity={'cores': 100, 'mem':10240}, 
+                                                        leftMemoryPoolCapacity={'cores': 50, 'mem':40960})
+        # only e1 should be split to respect limited core request in desiredServerPoolReduction
+        self.assertEqual(splittingDecisions, [1, 0, 0]) 
+        
+    
+    def test_NotTooMuchSplittingNeeded4(self):
+        e1 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 0.2], memUsageMetric= [52, 245], 
+            communicationMetric=[[0, 120], [120,0]])
+        e2 = Element(types=['c','m'], 
+            coreUsageMetric=[0.8, 0.5], memUsageMetric= [158, 652], 
+            communicationMetric=[[0, 840], [840,0]])
+        e3 = Element(types=['c','m'], 
+            coreUsageMetric=[1, 1], memUsageMetric= [26, 39], 
+            communicationMetric=[[0, 30], [30,0]])
+        splittingDecisions = self.splitter.SuggestSplit(splitCandidates=[e1, e2, e3], 
+                                                        desiredServerPoolReduction={'cores': 20, 'mem':65}, 
+                                                        leftCPUPoolCapacity={'cores': 100, 'mem':10240}, 
+                                                        leftMemoryPoolCapacity={'cores': 50, 'mem':40960})
+        # only e3 should be split to respect limited memory request in desiredServerPoolReduction
+        self.assertEqual(splittingDecisions, [0, 0, 1])
+
     def test_TooLittleMemoryLeftInCPUPool(self):
         e1 = Element(types=['c','m'], 
             coreUsageMetric=[1, 0.2], memUsageMetric= [52, 245], 
